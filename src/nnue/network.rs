f22f498b7b469aa35153be6828de247e07e4e4f6
warre <s0355721@gmail.com>
2026-08-12 23:20:24 +0200

    test inference bug solve bench   1682944

--- src/nnue/network.rs
+++ src/nnue/network.rs
@@ -38,15 +38,13 @@ pub struct BucketInfo {
 fn get_bucket(board: &Board, perspective: Color) -> BucketInfo {
     let king_sq = board.king_of(perspective).unwrap();
     let mut sq_idx = king_sq.to_usize();
-    if perspective == Color::Black {
-        sq_idx ^= 0b111000;
-    }
+
     let file = sq_idx & 0b111;
     let rank = sq_idx >> 3;
     let mirror = file >= 4;
     let bucket_file = if mirror { 7 - file } else { file };
     let bucket = KING_BUCKET_LAYOUT[rank * 4 + bucket_file];
-    
+
     BucketInfo { mirror, bucket }
 }
 
