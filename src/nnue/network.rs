a5c5b7e859ae26f2cb23004238f6e6325737bc01
warre <s0355721@gmail.com>
2026-08-14 08:53:39 +0200

    new inference that I built for kb seems to be faster bench 1569423

--- src/nnue/network.rs
+++ src/nnue/network.rs
@@ -5,23 +5,14 @@ const QB: i16 = 64;
 
 const NUM_OUTPUT_BUCKETS : usize = 8;
 
-const KING_BUCKET_LAYOUT: [usize; 64] =  [
-    0, 0, 1, 1, 5, 5, 4, 4,
-    2, 2, 2, 2, 6, 6, 6, 6,
-    3, 3, 3, 3, 7, 7, 7, 7,
-    3, 3, 3, 3, 7, 7, 7, 7,
-    3, 3, 3, 3, 7, 7, 7, 7,
-    3, 3, 3, 3, 7, 7, 7, 7,
-    3, 3, 3, 3, 7, 7, 7, 7,
-    3, 3, 3, 3, 7, 7, 7, 7,
-];
-const NUM_INPUT_BUCKETS: usize = 8;
+const KING_BUCKET_LAYOUT: [usize; 64] =  [0;64];
+const NUM_INPUT_BUCKETS: usize = 1;
 
 use std::arch::x86_64::*;
 
 use shakmaty::{Board, Chess, Color, Position, Role};
 
-static NNUE: Network = unsafe { std::mem::transmute(*include_bytes!("../../nnue/files/kb/quantised.bin")) };
+static NNUE: Network = unsafe { std::mem::transmute(*include_bytes!("../../nnue/files/quantised.bin")) };
 
 // =====================================================================================================================//
 // NNUE NETWORK IS TRAINED BY THE BULLET CRATE AND CODE HAS BEEN REUSED FROM ONE OF THE EXAMPLES TO DO THE INFERENCE
