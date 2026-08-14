40cb80df30ce6967c529dfda8bceb98f7fad8fc4
warre <s0355721@gmail.com>
2026-08-14 22:00:26 +0200

    baseline net from selfplay 4 kb 1536 hl mirrored bench 1548566

--- src/nnue/network.rs
+++ src/nnue/network.rs
@@ -5,7 +5,7 @@ const QB: i16 = 64;
 
 const NUM_OUTPUT_BUCKETS : usize = 8;
 
-const OLD_KING_BUCKET_LAYOUT: [usize; 64] =  [
+const KING_BUCKET_LAYOUT: [usize; 64] =  [
     0, 0, 1, 1, 1, 1, 0, 0,
     2, 2, 2, 2, 2, 2, 2, 2,
     3, 3, 3, 3, 3, 3, 3, 3,
@@ -16,9 +16,7 @@ const OLD_KING_BUCKET_LAYOUT: [usize; 64] =  [
     3, 3, 3, 3, 3, 3, 3, 3,
 ];
 
-const KING_BUCKET_LAYOUT: [usize; 64] =  [0;64];
-
-pub const NUM_INPUT_BUCKETS: usize = 1;
+pub const NUM_INPUT_BUCKETS: usize = 4;
 
 use std::arch::x86_64::*;
 
